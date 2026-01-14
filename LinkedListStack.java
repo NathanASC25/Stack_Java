ad6abaa8dab182eef1e5b8cb82878f62b1ae58bf
NathanASC25 <javanathanjchin@gmail.com>
2026-01-14 00:10:01 -0500

    first commit

---
+++ LinkedListStack.java
@@ -0,0 +1,17 @@
+public class LinkedListStack {
+        private Node top;
+
+	private class Node {
+                int value;
+		Node next;
+        }
+	public LinkedListStack() {
+                this.top = null;
+	}
+	public void push(int value) {
+                Node newNode = new Node();
+		newNode.data = value;
+		newNode.next = top;
+		top = newNode;
+        }
+}
